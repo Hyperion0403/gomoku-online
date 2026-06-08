c128263271d518830f84a3019c43633f19c3d939
hyperion <xzhou3107@163.com>
2026-06-08 10:49:32 +0800

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GOMOKU_SUPABASE = {
-  url: "https://xyojkpxlbuklgmyluiod.supabase.co/rest/v1/",
+  url: "https://xyojkpxlbuklgmyluiod.supabase.co",
   anonKey: "sb_publishable_ULBgvNsLYlcbBDatkmr1lg_upA8aq8o",
 };
